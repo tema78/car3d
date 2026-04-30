2ccf960598b4fe23b64ce65e5e9895c5ef08ffe7
tema78 <153671233+tema78@users.noreply.github.com>
2026-04-30 17:10:53 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,1 +1,27 @@
 
+const models = [
+  {
+    id: "bmw_m5_3d",
+    name: "BMW M5 3D Model",
+    price: 25,
+    seller: "AutoStudio",
+    file: "https://modelviewer.dev/shared-assets/models/Astronaut.glb",
+    category: "car"
+  },
+  {
+    id: "audi_rs7_3d",
+    name: "Audi RS7 3D Model",
+    price: 30,
+    seller: "3D Garage",
+    file: "https://modelviewer.dev/shared-assets/models/RobotExpressive.glb",
+    category: "car"
+  },
+  {
+    id: "merc_gt_3d",
+    name: "Mercedes AMG GT 3D",
+    price: 35,
+    seller: "AutoStudio",
+    file: "https://modelviewer.dev/shared-assets/models/FlightHelmet.glb",
+    category: "car"
+  }
+];
